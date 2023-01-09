6f4d448354c955eaa44e6ccce792788b5d4323c8
brmrodrigues <lpbruno12@gmail.com>
2023-01-09 14:39:39 -0300

    kitchen init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kitchen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kitchen/core_test.clj
@@ -0,0 +1,7 @@
+(ns kitchen.core-test
+  (:require [clojure.test :refer :all]
+            [kitchen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
